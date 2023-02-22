a2f082cc2317135230478484c5d6b2bb0030aa83
AngelArizpe <124701792+AngelArizpe@users.noreply.github.com>
2023-02-21 18:37:20 -0600

    Add files via upload

---
+++ EVA1_9_STACK_OVERFLOW/src/eva1_9_stack_overflow/EVA1_9_STACK_OVERFLOW.java
@@ -0,0 +1,11 @@
+package eva1_9_stack_overflow;
+
+public class EVA1_9_STACK_OVERFLOW {
+
+    public static void main(String[] args) {
+        A();
+    }
+    public static void A(){
+        A();
+    }
+}
