4745eb39efde6a10c7038f0f4960c9b4f36193d8
AngelArizpe <124701792+AngelArizpe@users.noreply.github.com>
2023-02-21 18:38:24 -0600

    Add files via upload

---
+++ EVA1_10_PARAMETROS/src/eva1_10_parametros/EVA1_10_PARAMETROS.java
@@ -0,0 +1,13 @@
+package eva1_10_parametros;
+
+public class EVA1_10_PARAMETROS {
+
+    public static void main(String[] args) {
+     nPrintln(5, "Hola");  
+    }
+    public static void nPrintln(String message, int n){
+        for (int i = 0; i < n; i++){
+            System.out.println(message);
+        }
+    }
+}
