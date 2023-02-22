796021aa8b1f8075e7e625b69c4932cfd039f738
AngelArizpe <124701792+AngelArizpe@users.noreply.github.com>
2023-02-21 18:34:12 -0600

    Add files via upload

---
+++ EVA1_7_METODOS1/src/eva1_7_metodos1/EVA1_7_METODOS1.java
@@ -0,0 +1,11 @@
+package eva1_7_metodos1;
+public class EVA1_7_METODOS1 {
+
+    public static void main(String[] args) {
+        myMethod(); //Invocar al metodo
+    }
+    //Creacion del metodo
+    public static void myMethod(){
+        System.out.println("You have called me, my name is myMethod");
+    }
+}
